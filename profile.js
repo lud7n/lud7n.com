4d675324af3628f9a4c84e9973ea2f197245beb0
lud7n <135094664+lud7n@users.noreply.github.com>
2024-01-30 04:38:16 +0900

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,106 @@
+document.querySelector('.container').animate(
+    [
+        { backgroundPosition: '0 0' },
+        { backgroundPosition: '-100% 0' },
+    ],
+    {
+        duration: 5000,
+        iterations: Infinity
+    }
+);
+
+document.querySelector('.container2').animate(
+    [
+        { backgroundPosition: '-100% 0' },
+        { backgroundPosition: '0 0' },
+    ],
+    {
+        duration: 5000,
+        iterations: Infinity
+    }
+);
+
+$(window).on('load', function () {
+    const videos = $('.js_inlineVideo');
+    if (videos.length) {
+        playVideos(videos);
+        $(window).on('scroll', function () {
+            playVideos(videos);
+        });
+    }
+    function playVideos(videos) {
+        const startPosition = $(window).scrollTop() + $(window).height();
+        videos.each(function (index) {
+            if (startPosition > $(this).offset().top - 200) {
+                $(this).get(0).play();
+            }
+        });
+    }
+});
+
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "css/style.css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
+
+$("#btn1").on("click", function () {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let message = `今日は${year}年${month}月${day}日です`
+    // jQueryを使って画面にメッセージを表示する
+    $("#tBox").val(message);
+});
---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>lud7n</title>
+</head>
+<body>
+<table border="1">
+  <tr>
+    <td>name</td><td><?php echo $_POST["name"] ?></td>
+  </tr>
+  <tr>
+    <td>mail</td><td><?php echo $_POST["mail"] ?></td>
+  </tr>
+</table>
+</body>
+</html>
